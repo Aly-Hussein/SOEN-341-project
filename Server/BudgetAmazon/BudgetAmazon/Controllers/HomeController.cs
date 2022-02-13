3d0aff16cb4054ee07a4a0cd993f7ce42dc32a59
yvich <98163289+yvich@users.noreply.github.com>
2022-02-13 00:53:33 -0500

    Update HomeControllers.cs
    adding add_to_cart and remove_from_cart options

--- Server/BudgetAmazon/BudgetAmazon/Controllers/HomeController.cs
+++ Server/BudgetAmazon/BudgetAmazon/Controllers/HomeController.cs
@@ -1,6 +1,15 @@
 ﻿using BudgetAmazon.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Newtonsoft.Json;
+using BudgetAmazon.DAL;
+using BudgetAmazon.Models.Home;
+using BudgetAmazon.Repository;
+using System.Web.Mvc;
 
 namespace BudgetAmazon.Controllers
 {
@@ -22,6 +31,66 @@ namespace BudgetAmazon.Controllers
         {
             return View();
         }
+          public IActionResult add_to_cart(int productId)
+        {
+            if (Session["cart"] == null)
+            {
+                List<Item> cart = new List<Item>();
+                var product = ctx.Tbl_Product.Find(productId);
+                cart.Add(new Item()
+                {
+                    Product = product,
+                    Quantity = 1
+                });
+                Session["cart"] = cart;
+            }
+            else
+            {
+                List<Item> cart = (List<Item>)Session["cart"];
+                var product = ctx.Tbl_Product.Find(productId);
+                foreach (var item in cart)
+                {
+                    if (item.Product.ProductId == productId)
+                    {
+                        int prevQty = item.Quantity;
+                        cart.Remove(item);
+                        cart.Add(new Item()
+                        {
+                            Product = product,
+                            Quantity = prevQty+1
+                        });
+                        break;
+                    }
+                    else
+                    {
+                        cart.Add(new Item()
+                        {
+                            Product = product,
+                            Quantity = 1
+                        });
+                    }
+                }
+                Session["cart"] = cart;
+            }
+            return Redirect("Index");
+        }
+
+        
+        public IActionResult remove_from_cart(int productId)
+        {
+            List<Item> cart = (List<Item>)Session["cart"];
+            foreach (var item in cart)
+            {
+                if (item.Product.ProductId == productId)
+                {
+                    cart.Remove(item);
+                    break;
+                }
+            }
+            Session["cart"] = cart;
+            return Redirect("Index");
+        }
+    }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
