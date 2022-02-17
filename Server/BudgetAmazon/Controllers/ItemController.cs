208b7ab0a97ef8dcf511af4b674459f1ce3f7572
Michael Younes <82108858+MKMicheal@users.noreply.github.com>
2022-02-16 21:04:50 -0500

    Testing new framework
    using asp.net framework instead of core

---
+++ Server/BudgetAmazon/Controllers/ItemController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BudgetAmazon.Models;
+using BudgetAmazon.ViewModel;
+
+namespace BudgetAmazon.Controllers
+{
+    public class ItemController : Controller
+    {
+        private BudgetAmazonEntities objBudgetAmazonEntities;
+        public ItemController()
+        {
+            objBudgetAmazonEntities = new BudgetAmazonEntities();
+
+        }
+        // GET: Item
+        public ActionResult Index()
+        {
+            ItemViewModel objItemViewModel = new ItemViewModel();
+            objItemViewModel.CategorySelectListItem = (from objCat in objBudgetAmazonEntities.Categories select new SelectListItem()
+                                                        {
+                                                        Text = objCat.CategoryName,
+                                                        Value = objCat.CategoryId.ToString(),
+                                                        Selected = true
+                                                        });
+            return View(objItemViewModel);
+        }
+    }
+}
---
+++ Server/BudgetAmazon/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BudgetAmazon.Startup))]
+namespace BudgetAmazon
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
