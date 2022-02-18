32fd61db0a07acc39e84829b257438ff12191537
Michael Younes <82108858+MKMicheal@users.noreply.github.com>
2022-02-17 21:43:17 -0500

    Cart feature
    Implemented the cart feature to be tested on pc with sql

--- Server/BudgetAmazon/Controllers/ShoppingController.cs
+++ Server/BudgetAmazon/Controllers/ShoppingController.cs
@@ -3,15 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using BudgetAmazon.Models;
+using BudgetAmazon.ViewModel;
 
 namespace BudgetAmazon.Controllers
 {
     public class ShoppingController : Controller
     {
+        private BudgetAmazonEntities objBudgetAmazonEntities;
         // GET: Shopping
+        public ShoppingController()
+        {
+            objBudgetAmazonEntities = new BudgetAmazonEntities();
+        }
         public ActionResult Index()
         {
-            return View();
+            IEnumerable<ShoppingViewModel> listOfShoppingViewModels = (from objItem in objBudgetAmazonEntities.Items
+                                                                       join  
+                                                                       objCate in objBudgetAmazonEntities.Categories
+                                                                       on objItem.CategoryId equals objCate.CategoryId
+                                                                       select new ShoppingViewModel()
+                                                                       {
+                                                                           ImagePath = objItem.ImagePath,
+                                                                           ItemName =  objItem.ItemName,
+                                                                           Description = objItem.Description,
+                                                                           ItemPrice = objItem.ItemPrice,
+                                                                           ItemId = objItem.ItemId,
+                                                                           Category = objCate.CategoryName,
+                                                                           ItemCode = objItem.ItemCode
+                                                                       }
+                                                                       ).ToList();
+            return View(listOfShoppingViewModels);
         }
     }
 }
