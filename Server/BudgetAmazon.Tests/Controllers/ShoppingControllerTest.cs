676613797ab24e28f08e9d3a53d192d69cc11b96
Jonathan Pauze <jonathanpauze25@gmail.com>
2022-04-03 15:36:35 -0400

    Setup Basic test infrastructure

---
+++ Server/BudgetAmazon.Tests/Controllers/ShoppingControllerTest.cs
@@ -0,0 +1,22 @@
+﻿using BudgetAmazon;
+using BudgetAmazon.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+
+namespace BudgetAmazon.Tests.Controllers
+{
+    [TestClass]
+    public class ShoppingControllerTest
+    {
+        [TestMethod]
+        public void AddOrder_scenario_expectedResult()
+        {
+            ShoppingController controller = new ShoppingController();
+
+        }
+    }
+}
