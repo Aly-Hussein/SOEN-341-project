428bb9f43e26d39a7db75904b13e310d6741f409
Brayden Tsitirides <brayden.tsitirides@cae.com>
2022-04-05 10:55:24 -0400

    Register Implementation
    Register now works when inserting into database, login failed error on this end but should work with correct login. Fails at SaveChanges() in accountcontroller/register

--- Server/BudgetAmazon/Models/Account.cs
+++ Server/BudgetAmazon/Models/Account.cs
@@ -11,9 +11,13 @@ namespace BudgetAmazon.Models
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
     public partial class Account
     {
+        [Key]
+        [Column(Order = 0)]
         public int CustomerId { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
