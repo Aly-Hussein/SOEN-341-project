c832df3a121090232c9568c6f56c3154cd7036a9
Brayden Tsitirides <brayden.tsitirides@cae.com>
2022-04-05 19:07:20 -0400

    Added Login Functionality
    Needs to be tested but functionality is there for logging in accounts

--- Server/BudgetAmazon/Controllers/AccountController.cs
+++ Server/BudgetAmazon/Controllers/AccountController.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
 using BudgetAmazon.Models;
+using BudgetAmazon.ViewModel;
+using System.Collections.Generic;
 
 namespace BudgetAmazon.Controllers
 {
@@ -71,27 +73,17 @@ namespace BudgetAmazon.Controllers
         [ValidateAntiForgeryToken]
         public async Task<ActionResult> Login(LoginViewModel model, string returnUrl)
         {
-            if (!ModelState.IsValid)
-            {
-                return View(model);
-            }
-
-            // This doesn't count login failures towards account lockout
-            // To enable password failures to trigger account lockout, change to shouldLockout: true
-            var result = await SignInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, shouldLockout: false);
-            switch (result)
-            {
-                case SignInStatus.Success:
-                    return RedirectToLocal(returnUrl);
-                case SignInStatus.LockedOut:
-                    return View("Lockout");
-                case SignInStatus.RequiresVerification:
-                    return RedirectToAction("SendCode", new { ReturnUrl = returnUrl, RememberMe = model.RememberMe });
-                case SignInStatus.Failure:
-                default:
-                    ModelState.AddModelError("", "Invalid login attempt.");
-                    return View(model);
-            }
+            IEnumerable<AccountViewModel> listOfShoppingViewModels = (from objItem in objBudgetAmazonEntities.Accounts
+                                                                       select new AccountViewModel()
+                                                                       {
+                                                                           Email = objItem.Email,
+                                                                           Password = objItem.Password,
+                                                                           IsAdmin = objItem.IsAdmin,
+                                                                           CustomerId = objItem.CustomerId
+                                                                       }
+                                                                      ).ToList();
+            return View(model);
+            
         }
 
         //
