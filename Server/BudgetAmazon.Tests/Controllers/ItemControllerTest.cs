9c50acef61053864b66a741b00f9454d5eb5d4a3
Jonathan Pauze <jonathanpauze25@gmail.com>
2022-04-07 15:19:36 -0400

    8 tests added
    Added 8 tests for the various controllers

---
+++ Server/BudgetAmazon.Tests/Controllers/ItemControllerTest.cs
@@ -0,0 +1,37 @@
+﻿using BudgetAmazon;
+using BudgetAmazon.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+
+namespace BudgetAmazon.Tests.Controllers
+{
+    [TestClass]
+    public class ItemControllerTest
+    {
+        [TestMethod]
+        public void Index_OnOpen_IsNotNull()
+        {
+            ItemController itemController = new ItemController();
+
+            var result = itemController.Index() as ActionResult;
+
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void Index_ExecutesProperly_StatusCodeOk()
+        {
+            var itemController = new ItemController();
+
+            var result = itemController.HttpContext.Response;
+
+            Assert.AreEqual(200, result.StatusCode);
+        }
+
+
+    }
+}
---
+++ Server/BudgetAmazon.Tests/Controllers/ShoppingControllerTest.cs
@@ -0,0 +1,46 @@
+﻿using BudgetAmazon;
+using BudgetAmazon.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+
+namespace BudgetAmazon.Tests.Controllers
+{
+    [TestClass]
+    public class ShoppingControllerTest
+    {
+        [TestMethod]
+        public void Index_OnOpen_IsNotNull()
+        {
+            ItemController itemController = new ItemController();
+
+            var result = itemController.Index() as ActionResult;
+
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void Index_ExecutesProperly_StatusCodeOk()
+        {
+            var shoppingController = new ShoppingController();
+
+            var result = shoppingController.HttpContext.Response;
+
+            Assert.AreEqual(200, result.StatusCode);
+        }
+
+        [TestMethod]
+        public void ShoppingCart_OnOpen_IsNotNull()
+        {
+            var cartController = new ShoppingController();
+
+            var result = cartController.ShoppingCart() as ActionResult;
+
+            Assert.IsNotNull(result);
+        }
+
+    }
+}
