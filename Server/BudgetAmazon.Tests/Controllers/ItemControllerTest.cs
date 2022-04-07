7045ad9778a40efbc2435bef51a93e10560cc173
Jonathan Pauze <jonathanpauze25@gmail.com>
2022-04-07 15:29:04 -0400

    Fixed tests
    Fixed faulty tests

--- Server/BudgetAmazon.Tests/Controllers/ItemControllerTest.cs
+++ Server/BudgetAmazon.Tests/Controllers/ItemControllerTest.cs
@@ -1,10 +1,13 @@
 ﻿using BudgetAmazon;
 using BudgetAmazon.Controllers;
+using BudgetAmazon.Models;
+using BudgetAmazon.ViewModel;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Web.Mvc;
 
 namespace BudgetAmazon.Tests.Controllers
@@ -12,24 +15,25 @@ namespace BudgetAmazon.Tests.Controllers
     [TestClass]
     public class ItemControllerTest
     {
-        [TestMethod]
-        public void Index_OnOpen_IsNotNull()
-        {
-            ItemController itemController = new ItemController();
-
-            var result = itemController.Index() as ActionResult;
-
-            Assert.IsNotNull(result);
-        }
 
         [TestMethod]
         public void Index_ExecutesProperly_StatusCodeOk()
         {
             var itemController = new ItemController();
+            ItemViewModel objItem = new ItemViewModel();
 
-            var result = itemController.HttpContext.Response;
+            objItem.ItemId = Guid.NewGuid();
+            objItem.CategoryId = 1;
+            objItem.ItemCode = "Test";
+            objItem.ItemName = "TestName";
+            objItem.Description = "Test";
+            objItem.ItemPrice = 10;
+            objItem.ImagePath = null;
+            objItem.CategorySelectListItem = null;
+            var result = itemController.Index(objItem);
+            var data = result.Data.ToString();
 
-            Assert.AreEqual(200, result.StatusCode);
+            Assert.AreEqual(Regex.Replace("{Success = true, Message = Item is added Successfully.}".ToLower(), @"\s+", ""), Regex.Replace(data.ToLower(), @"\s+", ""));
         }
 
 
