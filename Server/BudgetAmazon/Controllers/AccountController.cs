9e67174d63810b70dc7a43b4594a3f2e1db87ddd
Brayden Tsitirides <brayden.tsitirides@cae.com>
2022-04-06 20:23:18 -0400

    Register Working
    Register method now works full and inserts into the DB correctly

--- Server/BudgetAmazon/Controllers/AccountController.cs
+++ Server/BudgetAmazon/Controllers/AccountController.cs
@@ -21,12 +21,14 @@ namespace BudgetAmazon.Controllers
 
         public AccountController()
         {
+            objBudgetAmazonEntities = new BudgetAmazonEntities3();
         }
 
         public AccountController(ApplicationUserManager userManager, ApplicationSignInManager signInManager )
         {
             UserManager = userManager;
             SignInManager = signInManager;
+            objBudgetAmazonEntities = new BudgetAmazonEntities3();
         }
 
         public ApplicationSignInManager SignInManager
@@ -152,21 +154,15 @@ namespace BudgetAmazon.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
-                var result = await UserManager.CreateAsync(user, model.Password);
-                if (result.Succeeded)
-                {
-                    await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
-                    
-                    // For more information on how to enable account confirmation and password reset please visit https://go.microsoft.com/fwlink/?LinkID=320771
-                    // Send an email with this link
-                    // string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
-                    // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
-                    // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
+                Account newUser = new Account();
+                newUser.Email = model.Email;
+                newUser.CustomerId = Guid.NewGuid();
+                newUser.Password = model.Password;
+                newUser.IsAdmin = false;
+                objBudgetAmazonEntities.Accounts.Add(newUser);
+                objBudgetAmazonEntities.SaveChanges();
+                return RedirectToAction("Index", "Home");
 
-                    return RedirectToAction("Index", "Home");
-                }
-                AddErrors(result);
             }
 
             // If we got this far, something failed, redisplay form
