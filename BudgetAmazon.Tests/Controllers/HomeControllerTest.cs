b20d5953eb59d2171d2b16675edfe92a91fb36c1
Jonathan Pauze <jonathanpauze25@gmail.com>
2022-04-08 11:43:18 -0400

    Added remaining Test
    added more homeContoller and ManageController tests

--- BudgetAmazon.Tests/Controllers/HomeControllerTest.cs
+++ BudgetAmazon.Tests/Controllers/HomeControllerTest.cs
@@ -12,19 +12,45 @@ namespace BudgetAmazon.Tests.Controllers
     [TestClass]
     public class HomeControllerTest
     {
-       
+
         [TestMethod]
         public void Index()
         {
-            // Arrange
             HomeController controller = new HomeController();
 
-            // Act
             ViewResult result = controller.Index() as ViewResult;
 
-            // Assert
             Assert.IsNotNull(result);
         }
 
+        [TestMethod]
+        public void About_OnOpen_IsNotNull()
+        {
+            var controller = new HomeController();
+
+            var result = controller.About() as ViewResult;
+
+            Assert.IsNotNull(result);
+        }
+        
+        [TestMethod]
+        public void Contact_OnOpen_IsNotNull()
+        {
+            var controller = new HomeController();
+
+            var result = controller.Contact() as ViewResult;
+
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void Service_OnOpen_IsNotNull()
+        {
+            var controller = new HomeController();
+
+            var result = controller.Service() as ViewResult;
+
+            Assert.IsNotNull(result);
+        }
     }
 }
---
+++ BudgetAmazon.Tests/Controllers/ManageControllerTest.cs
@@ -0,0 +1,38 @@
+﻿using BudgetAmazon;
+using BudgetAmazon.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+
+namespace BudgetAmazon.Tests.Controllers
+{
+    [TestClass]
+    public class ManageControllerTest
+    {
+
+        [TestMethod]
+        public void SetPassword_OnOpen_IsNotNull()
+        {
+            var controller = new ManageController();
+
+            var result = controller.SetPassword() as ViewResult;
+
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+
+        public void ChangePassword_OnOpen_IsNotNull()
+        {
+            var controller = new ManageController();
+
+            var result = controller.ChangePassword() as ViewResult;
+
+            Assert.IsNotNull(result);
+        }
+
+    }
+}
